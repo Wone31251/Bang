4167e46d562eb3feae7a3b75cde7b5d14964d88f
Wone31251 <95580557+Wone31251@users.noreply.github.com>
2023-03-12 11:05:10 +0200

    Add files via upload
    Test

---
+++ test.php
@@ -0,0 +1,159 @@
+<html>
+    <head>
+        <title>Tetst site</title>
+        
+        <link rel="shortcut icon" href="assets/image/chrome_0dMhXCv9Ac.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="assets/image/favicon-32x32.png"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+        
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="">
+        <link rel="stylesheet" href="assets/css/style2.css" type="text/css">
+        <meta charset="utf-8">  
+
+        <!--Footer animations icons-->
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"> </script>  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"> </script>  
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"> </script>  
+   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" >  
+  
+    <!--FONT AWESOME-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<!--GOOGLE FONTS-->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Play&display=swap" rel="stylesheet"> 
+</head>
+
+   <!--footer-->
+    
+        
+    </head>
+
+<body >
+<header class="header">
+        <nav class="navbar">
+            
+    <img src="assets/image/dwarfjpg.jpg">
+            <ul class="nav-menu">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">About</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Contact</a>
+                </li>
+            </ul>
+            <div class="hamburger">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+        </nav>
+</header>
+
+
+   <!-- Section using styles -->
+   <div class="container1" class="">
+        <center><p1><h2 class="retroshadow">A litle bit settings</h2></p1></center>
+        <p1>
+           <code>.daasdasdas</code> Make this is site, is simple.
+           <code>.container</code> It's site reforged 
+            because I don't like old design this is site,
+            but I will be working for this is site.
+              
+            <br><br>
+                      <figure><code>https://github.com/Wone31251/Bang It's my github</code></figure>
+        </p1>
+    </div>
+    
+
+
+  <div class="footer">
+  <p>Footer</p>
+
+  <footer class="text-center text-white" style="background-color: #f1f1f1;">
+  <!-- Grid container -->
+  <div class="container pt-4">
+    <!-- Section: Social media -->
+    <section class="mb-4">
+      <!-- Facebook -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-facebook-f"></i
+      ></a>
+
+      <!-- Twitter -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-twitter"></i
+      ></a>
+
+      <!-- Google -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-google"></i
+      ></a>
+
+      <!-- Instagram -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-instagram"></i
+      ></a>
+
+    
+      <!-- Github -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-github"></i
+      ></a>
+    </section>
+    <!-- Section: Social media -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2023 Company Dwarf: All rights reserved. 
+    <a class="text-dark" href="https://www.youtube.com/channel/UCnOv33gUE87tqRjkpytqYUA">Youtube channel.</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+
+
+</div>
+</div>
+
+<!--js-->
+<script src="assets/js/main.js"></script> 
+
+
+</body>
+
+</html>
